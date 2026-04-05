c05175eb5c7c22e7406850bffa12faec20e87740
Kazu4042 <koseikazu@icloud.com>
2026-04-05 22:17:55 +0900

    骨格描画の追加

--- camera_ai_311.py
+++ camera_ai_311.py
@@ -2,10 +2,21 @@ import cv2
 import mediapipe as mp
 import time
 
+HAND_CONNECTIONS = [
+    (0,1),(1,2),(2,3),(3,4),
+    (0,5),(5,6),(6,7),(7,8),
+    (5,9),(9,10),(10,11),(11,12),
+    (9,13),(13,14),(14,15),(15,16),
+    (13,17),(17,18),(18,19),(19,20),
+    (0,17)
+]
+
 BaseOptions = mp.tasks.BaseOptions
 HandLandmarker = mp.tasks.vision.HandLandmarker
 HandLandmarkerOptions = mp.tasks.vision.HandLandmarkerOptions
 VisionRunningMode = mp.tasks.vision.RunningMode
+
+
 
 options = HandLandmarkerOptions(
     base_options=BaseOptions(model_asset_path="hand_landmarker.task"),
@@ -31,12 +42,20 @@ while cap.isOpened():
     result = landmarker.detect_for_video(mp_image, timestamp)
 
     if result.hand_landmarks:
-        print("検出！")
         for hand in result.hand_landmarks:
+            points = []
+            
+            # 点を描く
             for point in hand:
                 x = int(point.x * frame.shape[1])
                 y = int(point.y * frame.shape[0])
+                points.append((x, y))
                 cv2.circle(frame, (x, y), 5, (0, 255, 0), -1)
+
+            # 線を描く
+            for connection in HAND_CONNECTIONS:
+                start_idx, end_idx = connection
+                cv2.line(frame, points[start_idx], points[end_idx], (255, 0, 0), 2)
 
     cv2.imshow("Hand Tracking", frame)
 
